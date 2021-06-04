16a67415e251b7baa2acd5213741864b2e4f7234
GirlCode <k_przyborek@poczta.onet.pl>
2021-06-04 23:54:57 +0200

    Add script-title to HTML

---
+++ js/script-title.js
@@ -0,0 +1,23 @@
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+
+  /* for each article */
+
+    /* get the article id */
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+
+}
+
+generateTitleLinks();
